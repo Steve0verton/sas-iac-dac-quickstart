0215c06d7e0c72935fbdb0915b7718382f663226
Steve Overton <stephen.overton@gmail.com>
2025-01-19 15:01:25 -0500

    Add viya4-deployment script examples

---
+++ viya4-deployment/environments/env1/deploy_viya_software_base.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# NOTE: Execute within environment working directory
+
+docker run --rm \
+	--group-add root \
+	--user $(id -u):$(id -g) \
+	--volume $(pwd):/data \
+	--volume $HOME/deployments/viya4-iac-azure/environments/env1/env1-aks-kubeconfig.conf:/config/kubeconfig \
+	--volume $HOME/deployments/viya4-deployment/environments/env1/env1.yaml:/config/config \
+	--volume $HOME/deployments/viya4-iac-azure/environments/env1/env1.tfstate:/config/tfstate \
+	--volume $HOME/.ssh/env1:/config/jump_svr_private_key \
+	viya4-deployment --tags "baseline,viya,install" -vvv >> logs/deploy_$(date +"%FT%H%M").log
+
